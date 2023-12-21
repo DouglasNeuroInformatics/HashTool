f6614c7e31dc9aaad5cd18a39453706821c2cab3
joshunrau <contact@joshuaunrau.com>
2023-12-21 14:02:25 -0500

    add all

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
-import formsPLugin from '@tailwindcss/forms';
+import formsPlugin from "@tailwindcss/forms";
+import plugin from "tailwindcss/plugin";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class", '[data-mode="dark"]'],
+  plugins: [
+    formsPlugin,
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        ".text-muted": {
+          "@apply text-gray-700 dark:text-gray-300": {},
+        },
+      });
+    }),
   ],
   theme: {
     extend: {},
   },
-  plugins: [formsPLugin],
-}
+};
