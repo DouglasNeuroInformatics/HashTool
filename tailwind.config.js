1191432684210e84c6d9251de8138da76f25aa35
joshunrau <contact@joshuaunrau.com>
2023-12-21 12:41:00 -0500

    add interface

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import formsPLugin from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,5 +9,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [formsPLugin],
 }
