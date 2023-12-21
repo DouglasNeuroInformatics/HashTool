1a9d0c90896c6d5744afab760966dcee1da88b40
joshunrau <contact@joshuaunrau.com>
2023-12-21 12:53:08 -0500

    works

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-import './style.css'
+import { sha512 } from 'hash-wasm';
+
+import './style.css';
+
+const input = document.getElementById('input-text') as HTMLInputElement;
+const output = document.getElementById('output-text') as HTMLInputElement;
+
+input?.addEventListener('input', (event) => {
+  const value = (event.target as HTMLInputElement).value;
+  if (!value) {
+    output.value = '';
+  } else {
+    sha512(value).then((result) => {
+      output.value = result;
+    });
+  }
+});
