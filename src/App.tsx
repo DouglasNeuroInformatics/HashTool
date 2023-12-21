37ecc0d36585f9afe13778dc22d27fa148a3b729
joshunrau <contact@joshuaunrau.com>
2023-12-21 14:33:54 -0500

    nice

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,19 @@ export const App = () => {
         <h1 className="py-3 text-center text-xl font-bold">DNP Secure Hash</h1>
       </div>
       <Form
-        content={{
-          healthCardNumber: {
-            kind: 'text',
-            label: 'RAMQ Number',
-            variant: 'short'
+        content={[
+          {
+            title: 'Confidential Information',
+            description: 'For security reasons, this information is never saved on your computer or uploaded to a remote server.',
+            fields: {
+              healthCardNumber: {
+                kind: 'text',
+                label: 'RAMQ Number',
+                variant: 'short'
+              }
+            }
           }
-        }}
+        ]}
         onSubmit={(data) => alert(JSON.stringify(data))}
         validationSchema={z.object({
           healthCardNumber: z.string().min(1)
